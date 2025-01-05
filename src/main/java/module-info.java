7821cd00d401672e6279fd5410c7dc5217017b1a
Oussema Sahbeni <oussemasahbeni300@gmail.com>
2025-01-05 13:35:38 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.semesterproject_2022 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires org.apache.commons.codec;
+    requires com.mailjet.api;
+    requires AnimateFX;
+    requires org.jetbrains.annotations;
+
+    opens com.example.semesterProject_2022 to javafx.fxml;
+    exports com.example.semesterProject_2022;
+    opens model_class to javafx.fxml;
+    exports model_class;
+}
